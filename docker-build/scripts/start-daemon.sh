12334b39904319d81bd772e51ffdc0cfc5c85af1
goflutterjava <goflutterjava@gmail.com>
2022-12-04 16:25:58 +0800

    add alertmanager start script

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -2,3 +2,4 @@
 
 DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 mkdir -p $ALERTMANAGER_HOME/logs
+nohup $ALERTMANAGER_HOME/alertmanager >>$ALERTMANAGER_HOME/logs/alertmanager.stdout.log 2>>$ALERTMANAGER_HOME/logs/alertmanager.stderr.log &
